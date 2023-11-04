6ffe516c03b10099fe176f63d87496b703197065
TomaszSieminski <tomasz.sieminski.dev@gmail.com>
2023-11-04 22:46:21 +0100

    Launching the programme

---
+++ src/main/java/com/example/tetris/TetrisApplication.java
@@ -0,0 +1,17 @@
+package com.example.tetris;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TetrisApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        TetrisGame tetrisGame = new TetrisGame();
+        tetrisGame.start(primaryStage);
+    }
+}
---
+++ src/main/java/com/example/tetris/TetrisGame.java
@@ -0,0 +1,27 @@
+package com.example.tetris;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+public class TetrisGame extends Application {
+    public static final int BOARD_WIDTH = 10;
+    public static final int BOARD_HEIGHT = 20;
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Tetris Game");
+        Pane root = new Pane();
+        Scene scene = new Scene(root, BOARD_WIDTH * 30, BOARD_HEIGHT * 30);
+        scene.setFill(Color.DARKGRAY); // Set the background color
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
